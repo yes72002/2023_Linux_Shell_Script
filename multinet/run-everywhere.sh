cfc76a3d92c507bced5184903eff361d91d805f3
yes72002 <yes72002@gmail.com>
2023-01-11 21:01:12 +0800

    multinet commit

---
+++ multinet/run-everywhere.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# A list of servers, one per line.
+SERVER_LIST='/vagrant/servers'
+SSH_OPTIONS='-o ConnectTimeout=2'
+
+# Provides a usage statement much like you would find in a man page if the user does not
+# supply a command to run on the command line and returns an exit status of 1.
+# All messages associated with this event will be displayed on standard error.
+usage() {
+  echo "Usage: ${0} [-nsv] [-f FILE] COMMAND" >&2
+  echo 'Executes COMMAND as a single command on every server.' >&2
+  echo '  -f FILE Use FILE for the list of servers. Default: ${SERVER_LIST}' >&2
+  echo '  -n      Dry run mode. Display the COMMAND that would have been executed and exit.' >&2
+  echo '  -s      Execute the COMMAND using sudo on the remote server.' >&2
+  echo '  -v      Verbose mode. Displays the server name before executing COMMAND.' >&2
+  exit 1
+}
+
+log() {
+  local MESSAGE="${@}"
+  if [[ "${VERBOSE}" = 'true' ]]
+  then
+    echo "${MESSAGE}"
+  fi
+}
+
+# Enforces that it be executed without superuser (root) privileges. If the user wants the remote
+# commands executed with superuser (root) privileges, they are to specify the -s option.
+UID_OF_ROOT='0'
+if [[ "${UID}" -eq "${UID_OF_ROOT}" ]]
+then
+  echo 'Do not execute this script as root. Use the -s option instead.' >&2
+  usage
+  exit 1
+fi
+
+while getopts f:nsv OPTION
+do
+  case ${OPTION} in
+    f) SERVER_LIST="${OPTARG}" ;;
+    n) DRY_RUN='true' ;;
+    s) SUDO_OPTION='sudo' ;;
+    v) VERBOSE='true' ;;
+    ?) usage ;;
+  esac
+done
+
+# Remove the options while leaving the remaining arguments.
+shift "$(( OPTIND - 1 ))"
+
+# If the user doesn't supply at least one argument, give them help.
+if [[ "${#}" -lt 1 ]]
+then
+  usage
+fi
+
+# Anything that remains on the command line is to be treated as a single command.
+COMMAND="${@}"
+
+# Make sure the SERVER_LIST file exists.
+if [[ ! -e "${SERVER_LIST}" ]]
+then
+  echo "Cannot open server list file ${SERVER_LIST}." >&2
+  exit 1
+fi
+
+# Expect the best, prepare for the worst.
+EXIT_STATUS='0'
+
+# Loop through the SERVER_LIST
+for SERVER in $(cat ${SERVER_LIST})
+do
+  log "${SERVER}"
+  
+  SSH_COMMAND="ssh ${SSH_OPTIONS} ${SERVER} ${SUDO_OPTION} ${COMMAND}"
+  
+  if [[ "${DRY_RUN}" = 'true' ]]
+  then
+    echo "DRY RUN: ${SSH_COMMAND}"
+  else
+    ${SSH_COMMAND}
+    SSH_EXIT_STATUS="${?}"
+
+    # Capture any non-zero exit status from the SSH_COMMAND and report to the user.
+    if [[ "${SSH_EXIT_STATUS}" -ne 0 ]]
+    then
+      EXIT_STATUS=${SSH_EXIT_STATUS}
+      echo "Execution on ${SERVER} failed." >&2
+    fi
+  fi
+done
+
+exit ${EXIT_STATUS}
+
+
+
